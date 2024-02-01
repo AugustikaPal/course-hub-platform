ab4ed53ea20faa3641dcabce450bdc68c6966a73
Augustika <augustika1524@gmail.com>
2024-02-01 13:26:37 +0530

    react-courses-project

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,jsx,js}"],
+  theme: {
+    extend: {
+      backgroundColor :{
+        'bgDark': '#001f3f',
+       }
+    },
+  },
+  plugins: [],
+}
+
